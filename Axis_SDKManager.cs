ec3ec15a494f9d8b0ccdea376796be1e01d73cac
selena <matusevich.selena@gmail.com>
2024-12-17 17:46:13 +0200

    Initial commit - Axis LPR System

---
+++ Axis_SDKManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxisLPRSystem
+{
+    internal class Axis_SDKManager
+    {
+        private readonly HttpListener _listener;
+        private readonly int _port;
+
+        
+        public event Action<string> LicensePlateDetected;
+
+        public Axis_SDKManager(int port)
+        {
+            _port = port;
+            _listener = new HttpListener();
+            _listener.Prefixes.Add($"http://+:{_port}/api/LicensePlate/"); 
+        }
+
+        
+        public async void StartListening()
+        {
+            _listener.Start();
+            Console.WriteLine($"Listening for events on port {_port}...");
+
+            while (true)
+            {
+                var context = await _listener.GetContextAsync();
+                Task.Run(() => ProcessRequest(context));
+            }
+        }
+
+        
+        public void StopListening()
+        {
+            _listener.Stop();
+            Console.WriteLine("Stopped listening for events.");
+        }
+
+        
+        private async void ProcessRequest(HttpListenerContext context)
+        {
+            try
+            {
+                if (context.Request.HttpMethod == "POST")
+                {
+                    using (var reader = new System.IO.StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                    {
+                        string data = await reader.ReadToEndAsync();
+                        Console.WriteLine($"Received License Plate Data: {data}");
+
+                        
+                        LicensePlateDetected?.Invoke(data);
+                    }
+                }
+
+               
+                var response = context.Response;
+                string responseString = "Event received successfully";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+                response.OutputStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+
+
+namespace AxisLPRSystem
+{
+    public partial class Form1 : Form
+    {
+        private Axis_SDKManager _sdkManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            _sdkManager = new Axis_SDKManager(8090);
+
+            
+            _sdkManager.LicensePlateDetected += OnLicensePlateDetected;
+
+            
+            _sdkManager.StartListening();
+        }
+
+
+        private void OnLicensePlateDetected(string plateData)
+        {
+            try
+            {
+                
+                var json = JObject.Parse(plateData);
+                string plate = json["plate"]?.ToString() ?? "N/A";
+                string lane = json["lane"]?.ToString() ?? "N/A";
+                string time = DateTime.Now.ToString();
+
+                
+                Invoke(new Action(() =>
+                {
+                    listView1.Items.Add(new ListViewItem(new[]
+                    {
+                time,
+                lane,
+                plate
+            }));
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error parsing data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _sdkManager.StopListening();
+        }
+    }
+}
+
